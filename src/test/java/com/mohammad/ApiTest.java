ad40e88f830d4e5fcceda170de6693d593b74541
Mohammad Rahman <mzrahman74@gmail.com>
2026-07-08 18:07:14 -0500

    created new post call

--- src/test/java/com/mohammad/ApiTest.java
+++ src/test/java/com/mohammad/ApiTest.java
@@ -1,9 +1,13 @@
 package com.mohammad;
 
+import files.ReUsableMethod;
+import files.payload;
+import io.restassured.path.json.JsonPath;
 import org.testng.annotations.Test;
 import utils.ConfigReader;
 
 import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.when;
 
 
 public class ApiTest {
@@ -22,8 +26,31 @@ public class ApiTest {
                 .then()
                 .statusCode(200)
                 .extract()
-                .response().toString();
+                .response().getBody().prettyPrint();
 
         System.out.println("Response Body: " + res);
     }
+
+  @Test
+  public void testPostRequest() {
+    String res =
+        given()
+            .baseUri(ConfigReader.get("baseUrl"))
+            .contentType("application/json")
+            .header("x-api-key", ConfigReader.get("header_value"))
+            .body(payload.postUser())
+            .when()
+            .post("/users")
+            .then()
+            .assertThat()
+            .statusCode(201)
+            .extract()
+            .response()
+            .asString();
+    JsonPath js = ReUsableMethod.rawJson(res);
+    String userId = js.getString("id");
+    String meta = js.getString("_meta.powered_by");
+    System.out.println(userId);
+    System.out.println(meta);
+  }
 }
